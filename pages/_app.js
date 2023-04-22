46f3fd45006b97729f82ff83f3dae5bd4dfb8c67
ntk221 <nittakdub@gmail.com>
2023-04-22 12:25:36 +0900

    Twitterカードのメタデータを追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import Layout from "../components/layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
-import { Dosis} from "@next/font/google";
+import { Dosis } from "@next/font/google";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const dosis = Dosis({ subsets: ["latin"] });
 const theme = extendTheme({
@@ -13,12 +14,27 @@ const theme = extendTheme({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site" content="@YourTwitterHandle" />
+        <meta property="twitter:title" content="My Awesome Page" />
+        <meta
+          property="twitter:description"
+          content="This is a description of my awesome page"
+        />
+        <meta
+          property="twitter:image"
+          content="/images/me.JPG"
+        />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
-export default (MyApp);
+export default MyApp;
